2e7558d05c02500c88d09546906bcd8c29982f74
heejour <cuteprobe@gmail.com>
2017-07-03 21:55:04 +0900

    hihello

--- tasks.py
+++ tasks.py
@@ -1,6 +1,28 @@
 from microsoftbotframework import ReplyToActivity
+import requests
 
 def echo_response(message):
   if message["type"] == "message":
     ReplyToActivity(fill=message,
                     text=message["text"]).send()
+
+def bit_response():
+  ct = bit_contact()
+  if message["type"] == "message":
+    if message["text"] == "bitcoin"
+      ReplyToActivity(fill=message,
+                      text=ct).send()
+    else:
+      ReplyToActivity(fill=message,
+                      text=message["text"]).send()
+      
+def bit_contact():
+  res = requests.get("https://api.korbit.co.kr/v1/ticker")
+  t = str(res.text)
+  res_dict = eval(t)
+  
+  last = res_dict.get('last')
+  ct = "last cost : {}".format(last)
+  
+  print (ct)
+  return ct
